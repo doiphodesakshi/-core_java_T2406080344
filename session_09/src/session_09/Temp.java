ce9382800809809d892b9bb2a6b5728c8dc0057b
doiphodesakshi <128051915+doiphodesakshi@users.noreply.github.com>
2024-03-01 20:11:52 +0530

    commit

---
+++ session_09/src/session_09/Temp.java
@@ -0,0 +1,23 @@
+package session_09;
+
+public class Temp {
+
+	private int i;
+	
+	public Temp(int i) {
+		super();
+		this.i =i;
+	}
+	
+	@Override
+	public String toString() {
+		return i+"";
+	}
+	
+	@Override 
+	public int hashCode() {
+		return i;
+	}
+	
+	
+}
