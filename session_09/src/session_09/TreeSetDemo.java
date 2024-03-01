cba1bc77b857e6eb8b4424fe85c8591586a70f24
doiphodesakshi <128051915+doiphodesakshi@users.noreply.github.com>
2024-03-01 20:14:16 +0530

    commit

---
+++ session_09/src/session_09/TreeSetDemo.java
@@ -0,0 +1,18 @@
+package session_09;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+	
+	public static void main(String[] args) {
+		
+		TreeSet t = new TreeSet();
+		t.add("A");
+		t.add("a");
+		t.add("B");
+		t.add("z");
+		t.add("Z");
+		System.out.println(t);
+	}
+
+}
